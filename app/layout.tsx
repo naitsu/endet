f7a180ec7c449dc8d3a1d9ddc62f296c49a94744
Takashi Niimura <fliszt-s.141@hotmail.com>
2025-12-29 14:54:39 +0900

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "0000001",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Next.js and Supabase Starter Kit",
+  description: "desc-001",
 };
 
 const geistSans = Geist({
