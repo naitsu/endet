7cc066e75f0ca9f631d83142c94d381b89b7ebdc
Takashi Niimura <fliszt-s.141@hotmail.com>
2025-12-29 16:08:38 +0900

    updated

--- app/(public)/ping/page.tsx
+++ app/(public)/ping/page.tsx
@@ -1,3 +1,18 @@
-export default function Page() {
-    return <div>PUBLIC OK</div>;
-  }
+import fs from "fs/promises";
+import path from "path";
+import { remark } from "remark";
+import html from "remark-html";
+
+export default async function Page() {
+  const filePath = path.join(process.cwd(), "content", "hello.md");
+  const markdown = await fs.readFile(filePath, "utf-8");
+
+  const result = await remark().use(html).process(markdown);
+  const htmlContent = result.toString();
+
+  return (
+    <main style={{ padding: 24 }}>
+      <article dangerouslySetInnerHTML={{ __html: htmlContent }} />
+    </main>
+  );
+}
